01a713e93df6f297d16f58d9b2b2efddf0a0a4aa
Chris Morris <chrismorris87@hotmail.co.uk>
2021-11-21 12:48:50 +0000

    Adds Client directory

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { checkUserExists } = require('../db/mongodb.js');
 
-router.get('/', async (req, res) => {
-    const people = await checkUserExists();
-    res.json(people);
+// router.get('/', async (req, res) => {
+//     console.log("request received")
+//     const people = await checkUserExists();
+//     console.log(people);
+//     res.json(people);
+// })
+
+router.get('/hello', (req, res) => {
+    res.send("hello?");
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const api = require('./routes/api');
+// const api = require('./routes/api');
 
 const app = express();
 
-app.use('/api', api)
+// app.use('/api', api)
+
+app.get('/', (req, res) => {
+    console.log("hello")
+    res.send("Hello?")
+})
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
